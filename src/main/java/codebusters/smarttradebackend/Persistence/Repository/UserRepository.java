5186ef43208e1c2ff73121089d387113d7daee8a
Z3LEE <scelmar@etsinf.upv.es>
2024-04-11 20:04:54 +0200

    arreglado userRepository

--- src/main/java/codebusters/smarttradebackend/Persistence/Repository/UserRepository.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Repository/UserRepository.java
@@ -2,10 +2,12 @@ package codebusters.smarttradebackend.Persistence.Repository;
 
 import codebusters.smarttradebackend.BusinessLogic.Models.Users.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Query("select u from User u where u.Email = ?1 and u.Password = ?2")
     public User findByEmailAndPassword(String email, String password);
 }
