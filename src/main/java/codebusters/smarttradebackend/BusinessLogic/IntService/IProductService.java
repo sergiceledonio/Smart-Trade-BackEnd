4b545fd4524947450dfd34277ccc7a574e2ca6c4
Z3LEE <scelmar@etsinf.upv.es>
2024-05-19 20:49:21 +0200

    images

--- src/main/java/codebusters/smarttradebackend/BusinessLogic/IntService/IProductService.java
+++ src/main/java/codebusters/smarttradebackend/BusinessLogic/IntService/IProductService.java
@@ -2,6 +2,7 @@ package codebusters.smarttradebackend.BusinessLogic.IntService;
 
 import codebusters.smarttradebackend.BusinessLogic.Models.Products.Product;
 import codebusters.smarttradebackend.BusinessLogic.Models.Users.User;
+import org.springframework.web.multipart.MultipartFile;
 
 
 import java.awt.*;
@@ -43,4 +44,8 @@ public interface IProductService {
     public void validateProduct(Product product, boolean isValid);
 
     public String[] getAtrib(Product p);
+
+    void addImage(String name, MultipartFile image);
+
+    byte[] getImage(String name);
 }
