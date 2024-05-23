7c17898e4263db3f2b6db8771c39c32e9588566c
Z3LEE <scelmar@etsinf.upv.es>
2024-05-23 20:08:43 +0200

    orders finished

--- src/main/java/codebusters/smarttradebackend/BusinessLogic/Service/OrderService/OrderService.java
+++ src/main/java/codebusters/smarttradebackend/BusinessLogic/Service/OrderService/OrderService.java
@@ -2,6 +2,8 @@ package codebusters.smarttradebackend.BusinessLogic.Service.OrderService;
 
 
 import codebusters.smarttradebackend.BusinessLogic.IntService.IOrderService;
+import codebusters.smarttradebackend.BusinessLogic.Models.Order.Order;
+import codebusters.smarttradebackend.BusinessLogic.Models.Products.Product;
 import codebusters.smarttradebackend.BusinessLogic.Models.ShoppingCart.ShoppingCart;
 import codebusters.smarttradebackend.Persistence.Repository.OrderRepository.OrderRepository;
 import codebusters.smarttradebackend.Persistence.Repository.ShoppingCart.ShoppingCartRepository;
@@ -9,6 +11,8 @@ import codebusters.smarttradebackend.Persistence.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -24,7 +28,21 @@ public class OrderService implements IOrderService {
     private ShoppingCartRepository scdata;
 
     @Override
-    public void addOrder(int sc_id) {
-        Optional<ShoppingCart> sc = scdata.findById(sc_id);
+    public void addOrder(int user_id) {
+        Optional<ShoppingCart> sc = scdata.findByUserId(user_id);
+        Date now = new Date();
+        List<Product> productList = scdata.findShoppingProductsByUserId(user_id);
+        double total = 0;
+        for(int i = 0; i < productList.size(); i++) {
+            total += productList.get(i).getPrice();
+        }
+        Order o = new Order(sc.get(), now, "pending", total, "paypal");
+        scdata.delete(sc.get());
+        data.save(o);
+    }
+
+    @Override
+    public List<Order> getOrders(int user_id) {
+        return data.findByUserId(user_id);
     }
 }
--- src/main/java/codebusters/smarttradebackend/Persistence/Controllers/OrderController.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Controllers/OrderController.java
@@ -1,4 +1,31 @@
 package codebusters.smarttradebackend.Persistence.Controllers;
 
+import codebusters.smarttradebackend.BusinessLogic.Models.Order.Order;
+import codebusters.smarttradebackend.BusinessLogic.Service.OrderService.OrderService;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private OrderService service;
+
+    @PostMapping("/newOrder")
+    public void addOrder(@RequestBody Map<String, Integer> request) {
+        int user_id = request.get("user_id");
+        service.addOrder(user_id);
+    }
+
+    @GetMapping("/orders")
+    public List<Order> getOrders(@RequestBody Map<String, Integer> request) {
+        int user_id = request.get("user_id");
+        return service.getOrders(user_id);
+    }
 }
