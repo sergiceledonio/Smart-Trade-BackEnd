43c1f7c3c0acb89863f8b1dc715a114edd1d7d25
Brian Corredera Buendia <BCORBUE@upv.edu.es>
2024-05-23 21:03:41 +0200

    filtro precio

--- src/main/java/codebusters/smarttradebackend/Persistence/Controllers/SearchController.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Controllers/SearchController.java
@@ -1,12 +1,10 @@
 package codebusters.smarttradebackend.Persistence.Controllers;
 
 import codebusters.smarttradebackend.BusinessLogic.Models.Products.Product;
+import codebusters.smarttradebackend.BusinessLogic.Service.Product.ProductService;
 import codebusters.smarttradebackend.BusinessLogic.Service.Search.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -19,9 +17,13 @@ public class SearchController {
     @Autowired
     SearchService service;
 
-    @GetMapping("/price")
+    @Autowired
+    ProductService pservice;
+
+    @PostMapping("/price")
     public List<Product> sortByPrice(@RequestBody Map<String, Object> request) {
-        List<Product> Products = (List<Product>) request.get("Products");
+        //List<Product> Products = (List<Product>) request.get("Products");
+        List<Product> Products = pservice.getProducts();
         double minPrice = (double) request.get("minPrice");
         double maxPrice = (double) request.get("maxPrice");
         return service.sortByPrice(Products, minPrice, maxPrice);
@@ -36,7 +38,7 @@ public class SearchController {
 
     @GetMapping("/ascending")
     public List<Product> sortByAscendingCategory(@RequestBody Map<String, List<Product>> request) {
-        List<Product> Products = request.get("Products");
+        List<Product> Products = pservice.getProducts();
         return service.sortByAscendingCategory(Products);
     }
 
@@ -49,7 +51,7 @@ public class SearchController {
     @GetMapping("/storable")
     public List<Product> storableSearch(@RequestBody Map<String, Object> request) {
         List<Product> SearchHistory = (List<Product>) request.get("SearchHistory");
-        List<Product> Products = (List<Product>) request.get("Products");
+        List<Product> Products = pservice.getProducts();
         String searchText = (String) request.get("searchText");
         return service.storableSearch(SearchHistory, Products, searchText);
     }
