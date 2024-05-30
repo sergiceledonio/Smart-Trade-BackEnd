1252383a46c4095408f15276f9f02d85f7156b8c
IvanCrespoG <icregad@etsinf.upv.es>
2024-05-30 20:13:23 +0200

    card y paypal

--- src/main/java/codebusters/smarttradebackend/Persistence/Controllers/CardController.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Controllers/CardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/card")
 public class CardController {
 
     @Autowired
--- src/main/java/codebusters/smarttradebackend/Persistence/Controllers/PaypalController.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Controllers/PaypalController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/paypal")
 public class PaypalController {
 
     UserService userController;
