7350c75e15f2fc4c2df2d7fd97a8845ffa1faf81
Javier Chaler Romero <javichr11@gmail.com>
2024-06-04 12:12:29 +0200

    changes

--- src/main/java/codebusters/smarttradebackend/Persistence/Repository/WishList/WishProductRepository.java
+++ src/main/java/codebusters/smarttradebackend/Persistence/Repository/WishList/WishProductRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface WishProductRepository extends JpaRepository<WishProduct, Integer> {
-    @Query("SELECT wp FROM WishProduct wp WHERE wp.productWished.id = :p_id AND wp.wishList.id = :sc_id")
-    public WishProduct findByProduct(int p_id, int sc_id);
+    @Query("SELECT wp FROM WishProduct wp WHERE wp.productWished.id = :p_id AND wp.wishList.id = :wl_id")
+    public WishProduct findByProduct(int p_id, int wl_id);
 }
--- src/test/java/codebusters/smarttradebackend/SmartTradeBackEndApplicationTests.java
+++ src/test/java/codebusters/smarttradebackend/SmartTradeBackEndApplicationTests.java
@@ -10,6 +10,7 @@ import codebusters.smarttradebackend.Persistence.Repository.UserRepository;
 import codebusters.smarttradebackend.Persistence.Repository.WishList.WishListRepository;
 import codebusters.smarttradebackend.Persistence.Repository.WishList.WishProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -40,65 +41,57 @@ class SmartTradeBackEndApplicationTests {
 
     @InjectMocks
     private WishListService wldata;
+    User user;
+    Product product;
+    WishList wishList;
+    WishProduct wishProduct;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test //Testeo del método findProductsByUserId(); Recoge los productos asociados al id de usuario y los esperados y en el assertEquals comprueba que sean iguales
+    @Test
+    @DisplayName("TEST 1")
+    //Testeo del método findProductsByUserId(); Recoge los productos asociados al id de usuario y los esperados y en el assertEquals comprueba que sean iguales
     void test1() {
-        int userId = 1;
         List<Product> expectedProducts = new ArrayList<>();
-        when(wlrepo.findProductsByUserId(userId)).thenReturn(expectedProducts);
-        List<Product> actualProducts = wldata.getWishProductsById(userId);
+        when(wlrepo.findProductsByUserId(1)).thenReturn(expectedProducts);
+        List<Product> actualProducts = wldata.getWishProductsById(1);
         assertEquals(expectedProducts, actualProducts);
     }
-    @Test //Testeo del método addWishedProduct(); Añade un producto a un usuario que no estaba en la lista anteriormente y después comprueba que el método devuelva 1(Añadido correctamente)
+    @Test
+    @DisplayName("TEST 2")
+    //Testeo del método addWishedProduct(); Añade un producto a un usuario que no estaba en la lista anteriormente y después comprueba que el método devuelva 1(Añadido correctamente)
     void test2() {
-        User user = new User("test@example.com", "John Doe", "password", "City", "Street", "123", null, null, "client", "12345678A", null, null);
-        Product product = new Product("Product Name", 100.0, "Type", "Description", false, false, 1, new byte[]{});
+        user = new User();
+        product = new Product();
 
         when(udata.findById(1)).thenReturn(Optional.of(user));
-        when(pdata.findById(452)).thenReturn(Optional.of(product));
+        when(pdata.findById(1)).thenReturn(Optional.of(product));
 
-        WishList wishList = new WishList(user);
+        wishList = new WishList(user);
         when(wlrepo.findByUserId(1)).thenReturn(Optional.of(wishList));
-        int result1 = wldata.addWishedProduct(1, 452);
+        int result1 = wldata.addWishedProduct(1, 1);
         assertEquals(1, result1);
    }
+   @Test
+   @DisplayName("TEST 3")
+   void test3(){
+       user = new User();
+       product = new Product();
+       wishList = new WishList(user);
+       wishProduct = new WishProduct(wishList, product);
 
-    @Test //Testeo del método addWishedProduct(); Añade un producto a un usuario que ya estaba en la lista anteriormente y después comprueba que el método devuelva -1(No se añade)
-    void test3() {
-        User user = new User("test@example.com", "John Doe", "password", "City", "Street", "123", null, null, "client", "12345678A", null, null);
-        Product product = new Product("Product Name", 100.0, "Type", "Description", false, false, 1, new byte[]{});
-        WishList wishList = new WishList(user);
+       when(udata.findById(1)).thenReturn(Optional.of(user));
+       when(pdata.findById(1)).thenReturn(Optional.of(product));
+       when(wlrepo.findByUserId(1)).thenReturn(Optional.of(wishList));
+       when(wpdata.findByProduct(product.getId(), wishList.getId())).thenReturn((wishProduct));
 
-        when(udata.findById(1)).thenReturn(Optional.of(user));
-        when(pdata.findById(452)).thenReturn(Optional.of(product));
+       int result = wldata.addWishedProduct(1, 1);
 
-        when(wlrepo.findByUserId(1)).thenReturn(Optional.of(wishList));
-        int result2 = wldata.addWishedProduct(1, 452);
-        assertEquals(-1, result2);
+       assertEquals(-1, result);
    }
-//   @Test //Testeo del método delete(); Intenta eliminar el wishproduct seleccionado(Este si que existe)
-//    void test4(){
-//        int userId = 1;
-//        String productName = "Product Name";
-//        User user = new User("test@example.com", "John Doe", "password", "City", "Street", "123", null, null, "client", "12345678A", null, null);
-//        Product product = new Product("Product Name", 100.0, "Type", "Description", false, false, userId, new byte[]{});
-//        WishList wishList = new WishList(user);
-//
-//        WishProduct wishProduct = new WishProduct(wishList, product);
-//
-//        when(pdata.findProductByName(productName)).thenReturn(Optional.of(product));
-//        when(wpdata.findByProduct(product.getId(), wishList.getId())).thenReturn(wishProduct);
-//
-//
-//        wldata.delete(userId, productName);
-//        verify(wpdata).deleteById(wishProduct.getId());
-//
-//   }
 
 
 
